12f5c83ae5a7aa5c8ea58cdc46d58670e28c3c6e
Jennifer Wolan <jenniferwolan@gmail.com>
2022-12-18 09:55:34 -0700

    commit tree c63e53feac4395a280198e93bcc190fc20223123

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+} 
+function appendCat(name) {
+    const newCat = [...cats];
+    newCat.push(name)
+    return newCat; //return [...cats, name]
+}
+
+function prependCat(name) {
+    const newCat2 = [...cats];
+    newCat2.unshift(name);
+    return newCat2;
+}
+
+function removeLastCat() {
+    const newCat3 = [...cats];
+    newCat3.pop();
+    return newCat3;
+}
+function removeFirstCat() {
+    const newCat4 = [...cats];
+    newCat4.shift();
+    return newCat4;
+}
